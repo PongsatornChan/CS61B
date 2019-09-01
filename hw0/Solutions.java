1425efea455d0e26ce8f1fafe237c7947e110dc6
PONGSATORN CHANPANICHRAVEE <pongsatorn1612@berkeley.edu>
2019-09-01 15:01:42 -0700

    Completed threeSum and threeSumDistinct

--- hw0/Solutions.java
+++ hw0/Solutions.java
@@ -14,16 +14,39 @@ public class Solutions {
     return highest;
   }
   public static boolean threeSum(int[] numlst) {
-    return true;
-  }
+    for(int i = 0; i < numlst.length; i++) {
+      for(int j = 0; j < numlst.length; j++) {
+        for(int m = 0; m < numlst.length; m++) {
+          if(numlst[i]+numlst[j]+numlst[m] == 0) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  } 
 
   public static boolean threeSumDistinct(int[] numlst) {
-    return true;
+    for(int i = 0; i < numlst.length; i++) {
+      for(int j = 0; j < numlst.length; j++) {
+        for(int m = 0; m < numlst.length; m++) {
+          if(i!=j && i!=m && j!=m) {
+            if(numlst[i]+numlst[j]+numlst[m] == 0) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
   }
 
   public static void main(String[] args) {
     int[] numlst1 = {1, 2, 4, 25, -1, -2};
     System.out.println(max(numlst1));
+    System.out.println(threeSum(numlst1));
+    System.out.println(threeSum(new int[] {9, 2 , -7}));
+    System.out.println(threeSumDistinct(new int[] {6, -3, -2}));
   }
 
 }
