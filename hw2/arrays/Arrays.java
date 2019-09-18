be8984d10c1b5a17827cdcc806070789de0fcf64
Pongsatorn Chanpanichravee <pongsatorn1612@berkeley.edu>
2019-09-18 01:18:24 -0700

    Complete PartC & E

--- hw2/arrays/Arrays.java
+++ hw2/arrays/Arrays.java
@@ -6,22 +6,46 @@ package arrays;
 /** HW #2 */
 
 /** Array utilities.
- *  @author
+ *  @author Pongsatorn Chanpanichravee
  */
 class Arrays {
     /* C. */
     /** Returns a new array consisting of the elements of A followed by the
      *  the elements of B. */
     static int[] catenate(int[] A, int[] B) {
-        /* *Replace this body with the solution. */
-        return null;
+        int num = A.length + B.length;
+        int[] ab = new int[num];
+        for (int i = 0; i < num; i++) {
+            if (i < A.length) {
+                ab[i] = A[i];
+            } else {
+                ab[i] = B[i - A.length];
+            }
+        }
+        return ab;
     }
 
     /** Returns the array formed by removing LEN items from A,
      *  beginning with item #START. */
     static int[] remove(int[] A, int start, int len) {
-        /* *Replace this body with the solution. */
-        return null;
+        if (start >= A.length) {
+            return A;
+        }
+        int num;
+        if (A.length < start + len) {
+            num = start;
+        } else {
+            num = A.length - len;
+        }
+        int[] result = new int[num];
+        for (int i = 0; i < result.length; i++) {
+            if (i < start) {
+                result[i] = A[i];
+            } else {
+                result[i] = A[i + len];
+            }
+        }
+        return result;
     }
 
     /* E. */
@@ -31,7 +55,32 @@ class Arrays {
      *  returns the three-element array
      *  {{1, 3, 7}, {5}, {4, 6, 9, 10}}. */
     static int[][] naturalRuns(int[] A) {
-        /* *Replace this body with the solution. */
-        return null;
+        int num = 1;
+        int highest = A[0];
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] <= highest) {
+                num++;
+            }
+            highest = A[i];
+        }
+
+        int[][] result = new int[num][];
+        highest = A[0];
+        int startPos = 0;
+        int resultPos = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] <= highest) {
+                int[] sub = new int[i - startPos];
+                System.arraycopy(A, startPos, sub, 0, sub.length);
+                result[resultPos] = sub;
+                resultPos++;
+                startPos = i;
+            }
+            highest = A[i];
+        }
+        int[] sub = new int[A.length - startPos];
+        System.arraycopy(A, startPos, sub, 0, sub.length);
+        result[resultPos] = sub;
+        return result;
     }
 }
--- hw2/arrays/ArraysTest.java
+++ hw2/arrays/ArraysTest.java
@@ -1,15 +1,55 @@
 package arrays;
 
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/** FIXME
- *  @author FIXME
+/** Just another simple test for hw 2 part C
+ *  @author Pongsatorn Chanpanichravee
  */
 
 public class ArraysTest {
     /** FIXME
      */
+    @Test
+    public void testCatenate() {
+        int[] A = {1, 2, 3};
+        int[] B = {4, 5, 6};
+        int[] ab = {1, 2, 3, 4, 5, 6};
+        assertArrayEquals(ab, Arrays.catenate(A, B));
+
+        int[] empty = {};
+        assertArrayEquals(B, Arrays.catenate(empty, B));
+        assertArrayEquals(A, Arrays.catenate(A, empty));
+    }
+
+    @Test
+    public void testRemove() {
+        int[] A = {1, 2, 3, 4, 5};
+        int[] expect = {1, 5};
+        assertArrayEquals(expect, Arrays.remove(A, 1, 3));
+
+        int[] expect1 = {1, 2};
+        assertArrayEquals(expect1, Arrays.remove(A, 2, 4));
+
+        assertArrayEquals(A, Arrays.remove(A, 2, 0));
+        assertArrayEquals(A, Arrays.remove(A, 5, 2));
+    }
+
+    @Test
+    public void testNaturalRuns() {
+        int[] A = {1, 3, 7, 5, 4, 6, 9, 10};
+        int[][] expected = {{1, 3, 7}, {5}, {4, 6, 9, 10}};
+        assertArrayEquals(expected, Arrays.naturalRuns(A));
+
+        int[][] c = {{10}, {9}, {8}, {7}};
+        int[] d = {10, 9, 8, 7};
+        assertArrayEquals(c, Arrays.naturalRuns(d));
+
+        int[][] e = {{7, 8, 9, 10}};
+        int[] b = {7, 8, 9, 10};
+        assertArrayEquals(e, Arrays.naturalRuns(b));
+    }
 
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(ArraysTest.class));
