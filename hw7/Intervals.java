b548f450d86ff5d2e886a8b0beb63c2cfc2e0ed3
Pongsatorn Chanpanichravee <pongsatorn1612@berkeley.edu>
2019-11-20 23:59:46 -0800

    Finish part C

--- hw7/Intervals.java
+++ hw7/Intervals.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /** HW #7, Sorting ranges.
  *  @author
   */
--- hw7/Sum.java
+++ hw7/Sum.java
@@ -1,11 +1,24 @@
 /** HW #7, Two-sum problem.
- * @author
+ * @author Pongsatorn Chanpanichravee
  */
 public class Sum {
 
     /** Returns true iff A[i]+B[j] = M for some i and j. */
     public static boolean sumsTo(int[] A, int[] B, int m) {
-        return false;  // REPLACE WITH YOUR ANSWER
+        new MySortingAlgorithms.MergeSort().sort(A, A.length);
+        new MySortingAlgorithms.MergeSort().sort(B, B.length);
+        int i = 0;
+        int j = B.length - 1;
+        while(j != 0) {
+            if (A[i] + B[j] == m) {
+                return true;
+            } else if (A[i] + B[j] < m) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
     }
 
 }
