6380e897860046d35ee69f3da1696528d0f8fe3b
Pongsatorn Chanpanichravee <pongsatorn1612@berkeley.edu>
2019-11-22 13:25:59 -0800

    wrap up hw7

--- hw7/BST.java
+++ hw7/BST.java
@@ -17,7 +17,17 @@ public class BST {
      * // Provide a descriptive comment for this method here.
      */
     private BSTNode linkedListToTree(Iterator iter, int n) {
-        return null;
+        if (n == 0) {
+            return null;
+        } else if (n == 1) {
+            return new BSTNode(iter.next(), null, null);
+        } else {
+            BSTNode curr = new BSTNode();
+            curr.left = linkedListToTree(iter, n/2 );
+            curr.item = iter.next();
+            curr.right = linkedListToTree(iter, n/2 - 1);
+            return curr;
+        }
     }
 
     /**
@@ -44,6 +54,17 @@ public class BST {
      * Node for BST.
      */
     static class BSTNode {
+
+        public BSTNode() {
+            item = null;
+            left = null;
+            right = null;
+        }
+        public BSTNode(Object item, BSTNode left, BSTNode right) {
+            this.item = item;
+            this.left = left;
+            this.right = right;
+        }
 
         /** Item. */
         protected Object item;
